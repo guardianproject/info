495f85b19bf30e662a0b8bf1be9d3c01719c2cb0
Hans-Christoph Steiner <hans@eds.org>
2019-09-02 17:56:38 +0200

    generate an app page per language for all languages in fdroid index

--- setup-fdroid-data.py
+++ setup-fdroid-data.py
@@ -2,6 +2,8 @@
 
 import json
 import os
+import sys
+import yaml
 from datetime import datetime
 from fdroidserver import common
 from fdroidserver import index
@@ -16,10 +18,65 @@ def get_localized_icon(app):
     return 'https://guardianproject.info/fdroid/repo/icons-480/' + app['icon']
 
 
-config = dict()
-config['jarsigner'] = 'jarsigner'
-common.config = config
-index.config = config
+def get_languages(app):
+    ret = set()
+    if 'localized' in app:
+        languages = app['localized'].keys()
+        for language in languages:
+            if language in ('en', 'en-US', 'es-ES', 'es-US', 'fr-CA', 'fr-FR'):
+                continue
+            ret.add(language)
+    return sorted(ret)
+
+
+def write_app_page(app, languages, lang=None):
+    if lang:
+        filename = 'content/apps/%s.%s.md' % (app['packageName'], lang)
+    else:
+        filename = 'content/apps/%s.md' % app['packageName']
+    with open(filename, 'w') as fp:
+        fp.write('---\n')
+        fp.write('title: ' + app['name'] + ' ' + '\n')
+        if languages:
+            fp.write('languages: ' + yaml.safe_dump(languages, width=sys.maxsize, default_flow_style=True))
+        subtitle = app.get('summary')
+        lastUpdated = app.get('lastUpdated')
+        if lastUpdated:
+            ts = int(lastUpdated) / 1000
+            fp.write('date: ' + datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + ' ' + '\n')
+        fp.write('packageName: ' + app['packageName'] + '\n')
+        if 'localized' in app and 'en-US' in app['localized']:
+            if 'featureGraphic' in app['localized']['en-US']:
+               fp.write('#bigimg: [{"src": "' + 'https://guardianproject.info/fdroid/repo/'
+                        + app['packageName'] + '/en-US/'
+                        + app['localized']['en-US']['featureGraphic'] + '"},]\n')
+            if 'summary' in app['localized']['en-US']:
+                subtitle = app['localized']['en-US']['summary']
+        if 'localized' in app and lang in app['localized'] and 'summary' in app['localized'][lang]:
+            subtitle = app['localized'][lang]['summary']
+        if subtitle:
+            d = {'subtitle': subtitle}
+            fp.write(yaml.safe_dump(d, width=sys.maxsize, default_flow_style=False,
+                                    encoding='utf-8', allow_unicode=True).decode())
+        fp.write('icon: ' + get_localized_icon(app) + '\n')
+        if app.get('sourceCode'):
+            fp.write('sourceCode: ' + app['sourceCode'] + '\n')
+        fp.write('blackfriday:\n  extensions:\n    - hardLineBreak\n')
+        fp.write('tags:\n')
+        fp.write('  - app\n')
+        fp.write('  - ' + app['packageName'] + '\n')
+        fp.write('  - ' + app['name'].lower() + '\n')
+        fp.write('  - ' + app['license'] + '\n')
+        if lang is None:
+            fp.write('aliases:\n  - apps/' + app['name'].split(':')[0].lower() + '\n')
+            fp.write('menu:\n  main:\n    parent: apps\n')
+        fp.write('---\n\n')
+
+
+fdroid_config = dict()
+fdroid_config['jarsigner'] = 'jarsigner'
+common.config = fdroid_config
+index.config = fdroid_config
 
 headers = {'User-Agent': 'F-Droid'}
 timeout = 60
@@ -38,42 +95,10 @@ data, etag = index.download_repo_index(repo_url + '?fingerprint=' + fingerprint)
 appsdict = dict()
 for app in data['apps']:
     appsdict[app['packageName']] = app
-    if 'localized' in app:
-        languages = app['localized'].keys()
-    else:
-        languages = None
-    with open('content/apps/%s.md' % app['packageName'], 'w') as fp:
-        fp.write('---\n')
-        fp.write('title: ' + app['name'] + ' ' + '\n')
-        if languages:
-            fp.write('languages: [ ' + ', '.join(languages) + ' ]\n')
-        subtitle = app.get('summary')
-        lastUpdated = app.get('lastUpdated')
-        if lastUpdated:
-            ts = int(lastUpdated) / 1000
-            fp.write('date: ' + datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + ' ' + '\n')
-        fp.write('packageName: ' + app['packageName'] + '\n')
-        if 'localized' in app and 'en-US' in app['localized']:
-            if 'featureGraphic' in app['localized']['en-US']:
-               fp.write('#bigimg: [{"src": "' + 'https://guardianproject.info/fdroid/repo/'
-                        + app['packageName'] + '/en-US/'
-                        + app['localized']['en-US']['featureGraphic'] + '"},]\n')
-            if 'summary' in app['localized']['en-US']:
-                subtitle = app['localized']['en-US']['summary']
-        if subtitle:
-            fp.write('subtitle: ' + subtitle + '\n')
-        fp.write('icon: ' + get_localized_icon(app) + '\n')
-        if app.get('sourceCode'):
-            fp.write('sourceCode: ' + app['sourceCode'] + '\n')
-        fp.write('blackfriday:\n  extensions:\n    - hardLineBreak\n')
-        fp.write('tags:\n')
-        fp.write('  - app\n')
-        fp.write('  - ' + app['packageName'] + '\n')
-        fp.write('  - ' + app['name'].lower() + '\n')
-        fp.write('  - ' + app['license'] + '\n')
-        fp.write('aliases:\n  - apps/' + app['name'].split(':')[0].lower() + '\n')
-        fp.write('menu:\n  main:\n    parent: apps\n')
-        fp.write('---\n\n')
+    languages = get_languages(app)
+    write_app_page(app, languages)
+    for language in languages:
+        write_app_page(app, languages, language)
 
 data['apps'] = appsdict
 with open('data/repo.json', 'w') as fp:
